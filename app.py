ccfc51c507df87bb82a9fdd1fd83cfca9b87d7b2
JustinBear <justin850509@gmail.com>
2020-08-21 22:18:06 +0800

    0821

--- app.py
+++ app.py
@@ -3,10 +3,21 @@ from flask_socketio import SocketIO
 from flask_cors import CORS
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
-    return render_template('control.html')
+    return render_template('index.html')
+
+@socketio.on('client_event')
+def client_msg():
+    return None
+
+@socketio.on('connect_event')
+def connect_msg():
+    return None
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app, debug=True, host='192.168.50.23', port=5000)
