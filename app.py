12be96ff3ce85caf8bcad7b4358906ed533f39ed
JustinBear99 <53395475+JustinBear99@users.noreply.github.com>
2020-08-23 11:05:53 +0800

    0823

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template
+import os
+from importlib import import_module
+from flask import Flask, render_template, Response
 from flask_socketio import SocketIO
 from flask_cors import CORS
+
+if os.environ.get('CAMERA'):
+    Camera = import_module('camera_' + os.environ['CAMERA']).Camera
+else:
+    from camera import Camera
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -11,13 +18,19 @@ socketio = SocketIO(app)
 def index():
     return render_template('index.html')
 
-@socketio.on('client_event')
-def client_msg():
-    return None
+def gen(camera):
+    """Video streaming generator function."""
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-@socketio.on('connect_event')
-def connect_msg():
-    return None
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
     socketio.run(app, debug=True, host='192.168.50.23', port=5000)
